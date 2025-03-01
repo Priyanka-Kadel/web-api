48a39ae9c294171fa10065f1bf01743a304cf1c0
Priyanka-Kadel <kadelpriyanka21@gmail.com>
2025-03-01 13:29:39 +0545

    adding dashboard routes

--- routes/dashboardRoutes.js
+++ routes/dashboardRoutes.js
@@ -1,8 +1,8 @@
-const express = require('express');
-const router = express.Router();
-const { getStats } = require('../controllers/dashboardController'); // Import the getStats function from your controller
+// const express = require('express');
+// const router = express.Router();
+// const { getStats } = require('../controllers/dashboardController'); // Import the getStats function from your controller
 
-// Define the route to get stats
-router.get('/api/v1/stats', getStats); // Use the route path '/api/v1/stats' to handle the stats request
+// // Define the route to get stats
+// router.get('/api/v1/stats', getStats); // Use the route path '/api/v1/stats' to handle the stats request
 
-module.exports = router;
+// module.exports = router;
