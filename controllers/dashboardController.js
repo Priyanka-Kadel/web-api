c82cb8321ce6eeba66e211f6190f99bcd91d5ebf
Priyanka-Kadel <kadelpriyanka21@gmail.com>
2025-03-01 13:20:11 +0545

    removing unnecessary comments from dashboardController.js

--- controllers/dashboardController.js
+++ controllers/dashboardController.js
@@ -4,19 +4,16 @@ const Product = require('../models/product');
 // Combined getStats function
 exports.getStats = async (req, res) => {
   try {
-    // Count the total number of users
     const users = await User.countDocuments();
     
-    // Count the total number of products
     const products = await Product.countDocuments();
     
-    // Return all stats in the response
     const stats = {
       users,
       products,
     };
     
-    res.json(stats); // Sending all the stats as a response
+    res.json(stats);
   } catch (error) {
     res.status(500).json({ error: 'Server error' });
   }
