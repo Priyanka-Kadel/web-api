c0dea7355da6e61958c45648b3f8d8dcd74ea587
Priyanka-Kadel <kadelpriyanka21@gmail.com>
2025-03-01 13:19:18 +0545

    updating dashboardController.js

--- controllers/dashboardController.js
+++ controllers/dashboardController.js
@@ -1,23 +1,23 @@
-// const User = require('../models/customer');
-// const Product = require('../models/product');
+const User = require('../models/customer');
+const Product = require('../models/product');
 
-// // Combined getStats function
-// exports.getStats = async (req, res) => {
-//   try {
-//     // Count the total number of users
-//     const users = await User.countDocuments();
+// Combined getStats function
+exports.getStats = async (req, res) => {
+  try {
+    // Count the total number of users
+    const users = await User.countDocuments();
     
-//     // Count the total number of products
-//     const products = await Product.countDocuments();
+    // Count the total number of products
+    const products = await Product.countDocuments();
     
-//     // Return all stats in the response
-//     const stats = {
-//       users,
-//       products,
-//     };
+    // Return all stats in the response
+    const stats = {
+      users,
+      products,
+    };
     
-//     res.json(stats); // Sending all the stats as a response
-//   } catch (error) {
-//     res.status(500).json({ error: 'Server error' });
-//   }
-// };
+    res.json(stats); // Sending all the stats as a response
+  } catch (error) {
+    res.status(500).json({ error: 'Server error' });
+  }
+};
